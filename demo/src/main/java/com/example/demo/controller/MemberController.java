a3f263a0f61af6bf90a0836ceae4614192ba0fb5
redoyp <gsolu0302@gmail.com>
2025-04-03 15:53:12 +0900

    JPA API 연습 추가

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -5,7 +5,11 @@ import com.example.demo.service.MemberService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
@@ -22,5 +26,26 @@ public class MemberController {
     @GetMapping("/hi")
     public String htmlPage() {
         return "hi";
+    }
+
+    @ResponseBody // 여기서는 member를 응답으로 보내줌
+    @PostMapping("/members")
+    public Member saveMember(@RequestBody Member member) {
+        return memberService.insertMember(member);
+    }
+
+    // GET /members/{id} -> member 단건 조회
+    @ResponseBody
+    @GetMapping("/members/{id}")
+    public Member getMemberById(@PathVariable Long id) {
+        return memberService.getMemberById(id);
+    }
+
+    // DELETE /members/{id} -> 해당 id member 삭제 -> 삭제 성공 메시지 출력
+    @DeleteMapping("/members/{id}")
+    @ResponseBody
+    public String deleteMemberById(@PathVariable Long id) {
+        memberService.deleteMemberById(id);
+        return "삭제 성공";
     }
 }
--- demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/src/main/java/com/example/demo/service/MemberService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.repository.Member;
 import com.example.demo.repository.MemberRepository;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +21,22 @@ public class MemberService {
     // getMemberAll 멤버 정보를 모두 조회
     public List<Member> getMemberAll() {
         return memberRepository.findAll();
+    }
+
+    // member 테이블에 insert 쿼리
+    public Member insertMember(Member member) {
+        Member savedMember = memberRepository.save(member);
+        return savedMember;
+    }
+
+    // member 단건 조회
+    public Member getMemberById(Long id) {
+        Optional<Member> optMember = memberRepository.findById(id);
+        return optMember.orElse(new Member());  // id에 해당하는 값 없으면 빈 Member 객체 반환
+    }
+
+    // member id 삭제
+    public void deleteMemberById(Long id) {
+        memberRepository.deleteById(id);
     }
 }
